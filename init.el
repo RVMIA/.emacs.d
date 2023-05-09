5972956a00942ebbadefa455a55d6af267255e8a
ame <elliotdtx@gmail.com>
2023-05-08 19:39:40 -0500

    debugging themes

--- init.el
+++ init.el
@@ -8,16 +8,10 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(unless (package-installed-p 'liso-theme)
-  (package-refresh-contents)
-  (package-install 'liso-theme))
 ;;  DONT CHANGE ABOVE
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-
-
 
 ;; DONT CHANGE BELOW
 (custom-set-variables
@@ -25,11 +19,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(xresources))
+ '(custom-enabled-themes nil)
  '(custom-safe-themes
-   '("158f4e123878f1de2b5866ce8e1139a837dca8d4751a5a1c0b1f2bb1a06df05f" "77df8c957aea72f8d0f9710609163e0c957a477f95219b69a1a2b00b3f7f62ed" default))
+   '("19a2c0b92a6aa1580f1be2deb7b8a8e3a4857b6c6ccf522d00547878837267e7" "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" "158f4e123878f1de2b5866ce8e1139a837dca8d4751a5a1c0b1f2bb1a06df05f" "77df8c957aea72f8d0f9710609163e0c957a477f95219b69a1a2b00b3f7f62ed" default))
  '(package-selected-packages
-   '(xresources-theme haskell-mode popup-kill-ring exwm dmenu diminish spaceline org-bullets company dashboard rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode avy smex ido-vertical-mode beacon liso-theme which-key use-package)))
+   '(gruvbox-theme xresources-theme haskell-mode popup-kill-ring exwm dmenu diminish spaceline org-bullets company dashboard rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode avy smex ido-vertical-mode beacon liso-theme which-key use-package))
+ '(warning-suppress-log-types '((comp) (comp) (emacs)))
+ '(warning-suppress-types '((comp) (emacs))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
